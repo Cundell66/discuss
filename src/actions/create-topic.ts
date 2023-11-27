62072f5c49bbe1d13e961731a82998a6fc8c29e1
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-11-27 17:12:29 +0000

    create topic schema added

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,29 @@
 'use server'
 
-export async function createTopic() {
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+    name: z
+        .string()
+        .min(3)
+        .regex(/^[a-z-]+$/, {
+        message: 'Must be lowercase letters or dashes without spaces'
+        }),
+    description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+    const result = createTopicSchema.safeParse({ 
+        name: formData.get('name'),
+        description: formData.get('description')
+    });
+    if (!result.success){
+        console.log(result.error.flatten().fieldErrors);
+        
+    } else {
+        console.log(result.data.name, result.data.description);
+    }
+    
+
     // TODO: Revalidate HomePage
 }
