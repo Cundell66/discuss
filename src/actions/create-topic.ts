b08fa720da6e0201215eb400c19b04163a90738f
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-11-29 00:47:53 +0000

    added form state error

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { z } from "zod";
+import { auth } from "@/auth";
 
 const createTopicSchema = z.object({
     name: z
@@ -16,6 +17,7 @@ interface CreateTopicFormState {
     errors: {
         name?: string[];
         description?: string[];
+        _form?: string[];
     }
 }
 
@@ -30,10 +32,18 @@ export async function createTopic(
     if (!result.success){
         return{
         errors: result.error.flatten().fieldErrors,
-        }    
-    } else {
-        console.log(result.data.name, result.data.description);
+        }
     }
+
+    const session = await auth();
+    if (!session || !session.user)
+    {
+        return {
+            errors: {
+                _form: ['You must be signed in to do this'],
+            },
+        };
+    }    
     
 
     // TODO: Revalidate HomePage
